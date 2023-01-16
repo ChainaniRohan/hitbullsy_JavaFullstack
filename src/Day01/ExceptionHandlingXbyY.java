834155d9aaebcfe37ce117d624ac5a2a8326849a
it-sRohan <rohanchainani15@gmail.com>
2023-01-16 12:56:41 +0530

    first Commit

---
+++ src/Day01/ExceptionHandlingXbyY.java
@@ -0,0 +1,23 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class ExceptionHandlingXbyY {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+        try {
+            System.out.println(x/y);
+        }
+        catch (ArithmeticException e1){
+            System.out.println(y+" is Invalid number!");
+        }
+        catch(java.util.InputMismatchException e2){
+            System.out.println(x+" and "+y+" are not 32 bit signed Integer!");
+        }
+        catch(Exception e){
+            System.out.println("Something Went Wrong");
+        }
+    }
+}
---
+++ src/Day01/numPyramid.java
@@ -0,0 +1,32 @@
+package Day01;
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class numPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //first Pattern
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//        //second pattern
+//        for (int i = 1; i <= n; i++){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        //third pattern
+//        for (int i = n; i >= 1; i--){
+//            for (int j = 1; j <= i; j++){
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
